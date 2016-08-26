da6af0ab1012e6d03198566171ce006093ed7361
Wing Liu <wsl112@ray22.doc.ic.ac.uk>
2016-08-26 20:00:37 +0100

    Working

--- RR.cpp
+++ RR.cpp
@@ -20,7 +20,7 @@ void print_matrix(Mat<ZZ>& M);
 void print_vector(vec_ZZ v);
 void generate_random_p_vector(vec_ZZ& v, long n, long bit, ZZ seed);
 void generate_random_gh_matrix(mat_ZZ& M, int n);
-ZZ shortest_vector_found(mat_ZZ& M);
+ZZ shortest_vector_found(mat_ZZ& M, int& i);
 RR longest_vector_found(mat_ZZ& M);
 
 //Gaussseive
@@ -28,22 +28,23 @@ bool GSieve(mat_ZZ& M, ZZ goal_square);
 ZZ length_of_vector(vec_ZZ v);
 vec_ZZ Ksample(mat_ZZ B, mat_RR mu, vec_RR c, vec_RR sps);
 RR samplez(RR c, RR square,RR ln);
-bool update(vec_ZZ& c,list<vector_and_ls>& L, queue<vec_ZZ>& S);
+ZZ update(vec_ZZ& c,list<vector_and_ls>& L, queue<vec_ZZ>& S);
 bool reduce(vec_ZZ& v1, vec_ZZ& v2);
 
 int main(){
 
 srand(time(NULL));
 
-for (int n=30;n<=100;n+=5){
-mat_ZZ B,B1,B2;
+for (int n=30;n<=70;n+=2){
+mat_ZZ B,B1,B2,B3;
 B.SetDims(n,n);
 generate_random_gh_matrix(B,n);
 B1=B;
 B2=B;
+B3=B;
 LLL_RR(B1);
 BKZ_RR(B2);
-
+int index;
 
 //calculate goal square norm
 double gamma = tgamma((double)n/2+1);
@@ -63,57 +64,132 @@ ct=conv<RR>(0.5);
 RR gg;
 gg=1.05*gamma_r*d_r/pow(M_PI,0.5);
 gg*=gg;
-ZZ goal_square; 
+ZZ goal_square;
 goal_square=conv<ZZ>(gg);
 
 //run experiments
+
+clock_t start,end;
 vec_ZZ x;
 cout << "---------------------------------------------------------------Dimension " << n << "--------------------------------------------------------------------------------------------"<<endl;
+cout << "target norm: " << SqrRoot(goal_square) << endl;
+start=clock();
+LLL_RR(B1);
+end=clock();
+ZZ shortest;
+shortest=SqrRoot(shortest_vector_found(B1,index));
 cout << "LLL: ";
-if (LatticeSolve(x,B,B1[0])==1){        
-        cout << shortest_vector_found(B1) << endl;
+if (LatticeSolve(x,B,B1[index])==1){
+        cout << shortest;
 }
 else {
-        cout << shortest_vector_found(B1)<< "found but not a lattice vector" << endl;
-        print_vector(B1[0]);
+        cout << shortest << "found but not a lattice vector" << endl;
+        print_vector(B1[index]);
 }
+cout << " in time " << (end-start)/(double)CLOCKS_PER_SEC*1000 << " ms" << endl;
 
+start=clock();
+BKZ_RR(B2);
+end=clock();
+shortest=SqrRoot(shortest_vector_found(B2,index));
 cout << "BKZ: ";
-if (LatticeSolve(x,B,B2[0])==1){        
-        cout << shortest_vector_found(B2) << endl;
+if (LatticeSolve(x,B,B2[index])==1){
+        cout << shortest;
 }
 else {
-        cout << shortest_vector_found(B2)<< "found but not a lattice vector" << endl;
-        print_vector(B2[0]);
+        cout << shortest << "found but not a lattice vector" << endl;
+        print_vector(B2[index]);
 }
+cout << " in time " << (end-start)/(double)CLOCKS_PER_SEC*1000 << " ms" << endl;
 
-cout << "Gsieve: ";
-if (GSieve(B1,goal_square)){
-        vec_ZZ x;
-        if (LatticeSolve(x,B,B1[0])==1){        
-                cout << shortest_vector_found(B1) << endl;
-        }
-        else {
-                cout << shortest_vector_found(B1)<< "found but not a lattice vector" << endl;
-                print_vector(B1[0]);
-        }
+
+start=clock();
+BKZ_RR(B3,0.99,n,15);
+end=clock();
+shortest=SqrRoot(shortest_vector_found(B3,index));
+cout << "BKZ(prune+max_size): ";
+if (LatticeSolve(x,B,B3[index])==1){
+        cout << shortest;
 }
 else {
-	cout << "failed" << endl;
+        cout << shortest << "found but not a lattice vector" << endl;
+        print_vector(B3[index]);
 }
+cout << " in time " << (end-start)/(double)CLOCKS_PER_SEC*1000 << " ms" << endl;
+
+
+cout << "Gsieve(LLL): ";
+start=clock();
+if (GSieve(B1,goal_square)){
+        end=clock();
+        shortest=SqrRoot(shortest_vector_found(B1,index));
+        vec_ZZ x;
+        if (LatticeSolve(x,B,B1[index])==1){
+                cout << shortest;
+       }
+        else {
+                end=clock();
+                cout << shortest << "found but not a lattice vector" << endl;
+                print_vector(B1[index]);
+}
+}
+else {
+	cout << "failed";
+}
+cout << " in time " << (end-start)/(double)CLOCKS_PER_SEC*1000 << " ms" << endl;
+
+cout << "Gsieve(BKZ): ";
+start=clock();
+if (GSieve(B2,goal_square)){
+        end=clock();
+        shortest=SqrRoot(shortest_vector_found(B2,index));
+        vec_ZZ x;
+        if (LatticeSolve(x,B,B2[index])==1){
+                cout << shortest;
+       }
+        else {
+                end=clock();
+                cout << shortest << "found but not a lattice vector" << endl;
+                print_vector(B2[index]);
+}
+}
+else {
+	cout << "failed";
+}
+cout << " in time " << (end-start)/(double)CLOCKS_PER_SEC*1000 << " ms" << endl;
+
+cout << "Gsieve(BKZ(prune+max_size)): ";
+start=clock();
+if (GSieve(B3,goal_square)){
+        end=clock();
+        shortest=SqrRoot(shortest_vector_found(B3,index));
+        vec_ZZ x;
+        if (LatticeSolve(x,B,B3[index])==1){
+                cout << shortest;
+       }
+        else {
+                end=clock();
+                cout << shortest << "found but not a lattice vector" << endl;
+                print_vector(B3[index]);
+}
+}
+else {
+	cout << "failed";
+}
+cout << " in time " << (end-start)/(double)CLOCKS_PER_SEC*1000 << " ms" << endl;
+
 }
 
 /*
 string file_name;
-file_name="seed0-xx.txt";
+file_name="seed0-xx@.txt";
 
-for (int k=40;k<=70;k+=6){
-int n=k;
-mat_ZZ B,B1,B2;
+for (int n=40;n<=70;n+=2){
+mat_ZZ B,B2;
 
 B.SetDims(n,n);
 
-string s = to_string(k);
+string s = to_string(n);
 for (int y=0;y<s.length();y++){
         file_name[y+6]=s[y];
 }
@@ -122,18 +198,15 @@ ifstream input(file_name);
 if (input.is_open()){
 	for (int i=0;i<n;i++){
 		for (int j=0;j<n;j++){
-			input >> B[i][j];
+                        input >> B[i][j];
 		}
 	}
 	input.close();
 }
 
 
-B1=B;
 B2=B;
 
-LLL_RR(B1);
-BKZ_RR(B2);
 
 
 //calculate goal square norm
@@ -142,7 +215,7 @@ gamma=pow(gamma,1/(double)n);
 RR gamma_r;
 gamma_r = conv<RR>(gamma);
 ZZ d;
-d=abs(determinant(B1));
+d=abs(determinant(B2));
 RR d_r;
 d_r = conv<RR>(d);
 
@@ -154,45 +227,50 @@ ct=conv<RR>(0.5);
 RR gg;
 gg=1.05*gamma_r*d_r/pow(M_PI,0.5);
 gg*=gg;
-ZZ goal_square; 
+ZZ goal_square;
 goal_square=conv<ZZ>(gg);
 
 //run experiments
+int index;
 vec_ZZ x;
-cout << "LLL: ";
-if (LatticeSolve(x,B,B1[0])==1){        
-        cout << shortest_vector_found(B1) << endl;
-}
-else {
-        cout << shortest_vector_found(B1)<< "found but not a lattice vector" << endl;
-        print_vector(B1[0]);
-}
+cout << "---------------------------------------------------------------Dimension " << n << "--------------------------------------------------------------------------------------------"<<endl;
+cout << "target norm: " << SqrRoot(goal_square) << endl;
 
+clock_t start,end;
+start=clock();
+BKZ_RR(B2,0.99,n,15);
+ZZ shortest;
+shortest=SqrRoot(shortest_vector_found(B2,index));
 cout << "BKZ: ";
-if (LatticeSolve(x,B,B2[0])==1){        
-        cout << shortest_vector_found(B2) << endl;
+if (LatticeSolve(x,B,B2[index])==1){
+        cout << shortest << endl;
 }
 else {
-        cout << shortest_vector_found(B2)<< "found but not a lattice vector" << endl;
-        print_vector(B2[0]);
+        cout << shortest << "found but not a lattice vector" << endl;
+        print_vector(B2[index]);
 }
 
-cout << "Gsieve with LLL: ";
-if (GSieve(B1,goal_square)){
+cout << "Gsieve: ";
+if (GSieve(B2,shortest)){
+//if (GSieve(B2,goal_square)){
+        end=clock();
+        shortest=SqrRoot(shortest_vector_found(B2,index));
         vec_ZZ x;
-        if (LatticeSolve(x,B,B1[0])==1){        
-                cout << shortest_vector_found(B1) << endl;
-        }
+        if (LatticeSolve(x,B,B2[index])==1){
+                cout << shortest << endl;
+       }
         else {
-                cout << shortest_vector_found(B1)<< "found but not a lattice vector" << endl;
-                print_vector(B1[0]);
-        }
+                cout << shortest << "found but not a lattice vector" << endl;
+                print_vector(B2[index]);
+}
 }
 else {
 	cout << "failed" << endl;
 }
-}*/
 
+cout << "Total running time: " << (end-start)/(double)CLOCKS_PER_SEC*1000 << " ms" << endl;
+}
+*/
 return 0;
 }
 
@@ -200,7 +278,7 @@ return 0;
 void print_matrix(Mat<ZZ>& M){
 for (int i=0;i<M.NumRows();i++){
 	for (int j=0;j<M.NumCols();j++){
-		cout << M[i][j] << " ";	
+		cout << M[i][j] << " ";
 	}
 	cout << endl;
 }
@@ -241,15 +319,17 @@ for (int i=1;i<n;i++){
 }
 }
 
-ZZ shortest_vector_found(mat_ZZ& M){
+ZZ shortest_vector_found(mat_ZZ& M, int& j){
 ZZ length_square;
 InnerProduct(length_square,M[0],M[0]);
+j=0;
 for (int i=1;i<M.NumRows();i++){
 	ZZ length;
 	InnerProduct(length,M[i],M[i]);
 	if (length<length_square){
 		length_square=length;
-	}	
+                j=i;
+	}
 }
 return length_square;
 }
@@ -262,7 +342,7 @@ for (int i=1;i<M.NumRows();i++){
 	InnerProduct(length,M[i],M[i]);
 	if (length>length_square){
 		length_square=length;
-	}	
+	}
 }
 
 RR length;
@@ -277,8 +357,11 @@ return length;
 
 bool GSieve(mat_ZZ& M, ZZ goal_square){
 int n=M.NumRows();
+int index;
 ZZ min;
-min = shortest_vector_found(M);
+min = shortest_vector_found(M,index);
+vec_ZZ min_vector;
+min_vector = M[index];
 ZZ collisions ;
 ZZ max_list_size ;
 
@@ -309,8 +392,6 @@ for (int i=0;i<n;i++){
 	sps[i]=ssquare/c[i];
 }
 
-vec_ZZ min_vector;
-min_vector.SetLength(n);
 
 
 //update
@@ -320,13 +401,13 @@ for (int i=0;i<n;i++){
 max_list_size=L.size();
 //update max_list_size
 while ((min>goal_square) && (collisions<(ZZ)(0.1*max_list_size+(ZZ)200))){
-//cout << min << ">" << goal_square << "              " << collisions << "< " <<(0.1*max_list_size+(ZZ)200) << endl;  
+//cout << min << ">" << goal_square << "              " << collisions << "< " <<(0.1*max_list_size+(ZZ)200) << endl;
 	vec_ZZ current;
-        
+
         if (max_list_size < (ZZ)L.size()){
                 conv(max_list_size,L.size());
         }
-  //      cout << "here0" << endl; 
+  //      cout << "here0" << endl;
 
         if (!S.empty()){
 		current=S.front();
@@ -342,11 +423,11 @@ while ((min>goal_square) && (collisions<(ZZ)(0.1*max_list_size+(ZZ)200))){
 	cout << "]" << endl;
 	*/
 	//GaussReduce
-//        cout << "here1" << endl; 
+//        cout << "here1" << endl;
 	ZZ cns;
 	cns=update(current,L,S);
-	
-//        cout << "here2" << endl; 
+
+//        cout << "here2" << endl;
 	if(cns==(ZZ)0){
 		collisions++;
 	}
@@ -370,7 +451,7 @@ for (int i=0;i<n;i++){
 	temp+=v[i]*v[i];
 }
 return temp;
-}	
+}
 
 
 vec_ZZ Ksample(mat_ZZ B, mat_RR mu, vec_RR c, vec_RR sps){//check address
@@ -385,7 +466,7 @@ temp.SetLength(n);
 for (int i=n-1;i>=0;--i){
 	temp[i]=samplez(temp[i], sps[i], lnn);
 	for (int j=0;j<i;j++){
-		temp[j] -= (temp[i]*mu[i][j]);	
+		temp[j] -= (temp[i]*mu[i][j]);
 	}
 }
 //print_vector(temp);
@@ -416,13 +497,13 @@ while(true){
 	const RR r=-M_PI*(x-c)*(x-c)/square;
 	rho=exp(r);
 	if ((RR)rand()/RAND_MAX<=rho){//check
-		return x;	
+		return x;
 	}
 }
 }
 
 
-bool update(vec_ZZ& c,list<vector_and_ls>& L, queue<vec_ZZ>& S){
+ZZ update(vec_ZZ& c,list<vector_and_ls>& L, queue<vec_ZZ>& S){
 bool reduced = false;
 list<vector_and_ls>::iterator it,tit;
 vec_ZZ v;
@@ -432,7 +513,7 @@ while (!reduced){
 	reduced = true;
 	for (it=L.begin(); it!=L.end(); it++){
 		tie(v,ls)=*it;
-                //cout << ls << "<=" <<length_of_vector(c)<<endl;; 
+                //cout << ls << "<=" <<length_of_vector(c)<<endl;;
 		if (ls <= length_of_vector(c)){
 			if (reduce(c,v)){
 				reduced=false;
@@ -440,12 +521,15 @@ while (!reduced){
 		}
 	}
 }
-if (length_of_vector(c)==0){
-	cout << "collisions";
-	return false;
+ZZ length_of_c;
+length_of_c = length_of_vector(c);
+
+if (length_of_c==0){
+	//cout << "collisions";
+	return (ZZ)0;
 }
 vector_and_ls gggg;
-gggg=make_tuple(c,length_of_vector(c));
+gggg=make_tuple(c,length_of_c);
 L.insert(it,gggg);
 while (it!=L.end()){
 	tit=it;
@@ -459,18 +543,18 @@ while (it!=L.end()){
 	it++;
 }
 
-return true;
+return length_of_c;
 }
 
 bool reduce(vec_ZZ& v1, vec_ZZ& v2){
 long n=v1.length();
 ZZ ip;
 ip=v1*v2;
-//cout << abs(ip) << "<=" <<length_of_vector(v2) << endl; 
+//cout << abs(ip) << "<=" <<length_of_vector(v2) << endl;
 if (abs((ZZ)2*ip)<=length_of_vector(v2)){
 	return false;
 }
-RR ip_RR,ls_RR; 
+RR ip_RR,ls_RR;
 ip_RR=conv<RR>(ip);
 ls_RR=conv<RR>(length_of_vector(v2));
 
@@ -480,5 +564,5 @@ conv(q,r);
 for (long i=0;i<n;i++){
 	v1[i]-=v2[i]*q;
 }
-return true;	
+return true;
 }
